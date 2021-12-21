d542581fd309d46ffe3d230a079d766b443020d1
AmirthaSuganthi <amirthalakshmi1801@gmail.com>
2021-12-21 15:33:27 +0530

    c added

--- src/org/data/Data.java
+++ src/org/data/Data.java
@@ -3,7 +3,12 @@ package org.data;
 public class Data {
 
 	private void add() {
-		
+		System.out.println("Data method");
 		System.out.println("Add method");
 	}
+	
+	private void m3() {
+		System.out.println("m3 ,ethod");
+	}
+	
 }
